49fdf97567ee1ed9cd164fc7b91c39a026263c3a
Fasika1982 <168666859+Fasika1982@users.noreply.github.com>
2024-08-21 14:57:47 -0500

    Add files via upload

---
+++ buttonclicker/buttonclicker.js
@@ -0,0 +1,16 @@
+
+function btn2(element) {
+    element.innerText = "logout";
+    
+}
+
+function btn4(element) {
+    element.remove();
+    
+}
+
+function btn3(element) {
+    element.innerText = "ninja was liked";
+    
+}
+
